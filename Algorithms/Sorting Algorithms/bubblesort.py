6e6c057a342dd4c9697a8fb02119c7c8dabee7e2
bipintatkare2 <bipin.tatkare@policyera.com>
2021-08-30 11:24:24 +0530

    Bubble Sort

---
+++ Algorithms/Sorting Algorithms/bubblesort.py
@@ -0,0 +1,24 @@
+# Code is to Explain how Bubble sort works
+
+# Function for Bubble-Sort
+def bubbleSort(arr):
+    # Defining length of array
+    length = len(arr)
+    # For loop for first variable
+    for first_num in range(length-1):
+        # For loop for second variable
+        for second_num in range(length-first_num-1):
+            # If second number means current element is greater than its after-element
+            if arr[second_num] > arr[second_num+1]:
+                # Swap those elements with these formula
+                arr[second_num], arr[second_num+1] = arr[second_num+1], arr[second_num]
+
+
+# Creating a list
+bubble_list = [10, 30, 20, 50, 40, 70, 60, 100, 80, 90]
+
+# Function Calling
+bubbleSort(bubble_list)
+
+print(bubble_list)
+
